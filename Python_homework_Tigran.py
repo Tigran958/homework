88fa996768ea6d0b68697740f651f3a76a85fd18
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-18 00:19:53 +0400

    Homework with function input

--- Python_homework_Tigran.py
+++ Python_homework_Tigran.py
@@ -1,56 +1,31 @@
-#variables
-variable_in_type_bool = "True"
-boolean = type(bool(variable_in_type_bool))
-int_variable = 5
-int_variable_2 = 2
-int_variable_3 = 1
-int_variable_4 = 17
-float_variable = float("15.5")
-text_bool = "True"
-text_first = "Hello"
-text_2_variable = "Jiro jan"
-text_3_variable = "here is"
-text_4_variable = "my homework"
-t_break = " "
+# variables
+first_text = "Hello, lets do some calculation, I can show you an intersting fact"
+name_quest = "What if we get to know each other at first, tell me your name "
 
-#Operators that we have passed
-
-operator_pl = "+"
-operator_min = "-"
-operator_div = "/"
-operator_mult = "*"
-operator_2div = "//"
-operator_2mult = "**"
-operator_equal = "="
-operator_2equal = "=="
-
-#functions that we have passed
-function_print = "print()"
-function_type = "type()"
-function_str = "str()"
-function_int = "int()"
-function_float = "float()"
-
-#other variables
-result_0 = 5**2+2-5/1*5+(15.5//1)
-result_2_div = result_0 == int_variable_4
-result_1_div = 5**2+2-5/1*5+(15.5/1) == int_variable_4
-result_of_actions_2_div = str(int_variable) + operator_2mult + str(int_variable_2) + operator_pl + str(int_variable_2) + operator_min + str(int_variable) + operator_div + str(int_variable_3) + operator_mult + str(int_variable) + operator_pl + "(" + str(float_variable) + operator_2div + str(int_variable_3) + ")" + operator_2equal + str(int_variable_4)
-result_of_actions_div = str(int_variable) + operator_2mult + str(int_variable_2) + operator_pl + str(int_variable_2) + operator_min + str(int_variable) + operator_div + str(int_variable_3) + operator_mult + str(int_variable) + operator_pl + "(" + str(float_variable) + operator_2div + str(int_variable_3) + ")"
-type_of_int_variable = type(int_variable)
-
-print(text_first + t_break + text_2_variable + t_break + text_3_variable + t_break + text_4_variable) 
-print("We have learned about operators and some functions of Python, bellow is an example, hope you'll enjoy it")
-
-print(t_break + t_break + result_of_actions_2_div + t_break + t_break + "=>" + t_break + str(result_2_div)) 
-print("  If we use / instead of //, the result would be :" + t_break + str(result_1_div) + " as " + result_of_actions_div + operator_equal + "17.5" + ",  a way to change the value is to change " + str(type(float_variable)) + " format with " + str(type_of_int_variable))
-print("like : int(" + result_of_actions_div + ")") 
-
-
-
-
-
-
-
-
-
+#actions
+print(first_text)
+answer_first = input(name_quest)
+answer_name = "I'm really glad that we've met, " + answer_first
+print(answer_name)
+print("I can show you, how much your weight shuld be at the given height ")
+height_quest = input("So tell me, how toll are you in cm-s? ")
+height_float = float(height_quest)
+print(height_quest + " cm" + " is a good height!")
+weight_quest = input(" What about your weight and birth year, first how much do you weight in kilos? ")
+weight_int = int(weight_quest)
+date_birth_year = input("and the birth year? ")
+birth_year = 2019 - int(date_birth_year)
+print("Hmm " + weight_quest + " at " + str(birth_year) + "..., let me think")
+input("Ok I've finished, if you want to see the result, please push Enter bottom")
+index_bmi = weight_int/(height_float/100)**2
+import datetime
+date_of_act = datetime.datetime.now()
+print("So, " + answer_first + " in " + str(date_of_act)  + ", your body mass index (BMI) is -> " + str(index_bmi))
+input("There are 6 stages of BMI, push Enter and find your result bellow")
+print("	1. less than 18.5 -> Body mass deficit")
+print("	2. 18.5-24.9      -> Normal body mass")
+print("	3. 25.0-29.9      -> Excessive body mass")
+print("	4. 30.0-34.9      -> Obesity 1st degree")
+print("	5. 35.0-39.9      -> Obesity 2nd degree")
+print("	6. 40 and more    -> Obesity 3rd degree")
+print("Hope you liked it")
