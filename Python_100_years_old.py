85cdb613c0b1946ce574349c640f7fbe8129a007
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-23 20:19:24 +0400

    Your 100 age

---
+++ Python_100_years_old.py
@@ -0,0 +1,5 @@
+first_input = input("Tell your name ")
+second_input = input("Tell your birth year ")
+text_1 = "you will be 100 in "
+year_hundred = 100 - (2019 - int(second_input))
+print(text_1 + str(year_hundred))
