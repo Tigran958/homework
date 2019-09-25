c249fcde6dd1bab022dfa98a51b2b9b3438f21be
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-25 19:37:46 +0400

    Use if

---
+++ Python_if.py
@@ -0,0 +1,13 @@
+a = 7
+b = 7
+c = a + b
+g = "Hello"
+t = "Hello world"
+if a == b:
+	print("They are equals")
+if a > b:
+	print("a is great")
+if a == b and a < c:
+	print(t)
+if g in t and a == b:
+	print("Aloha")
