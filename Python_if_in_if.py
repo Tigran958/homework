924ac274db7fd7b7abc6179716d5e0a6d81565b4
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-25 20:23:09 +0400

    If with else

--- Python_if_in_if.py
+++ Python_if_in_if.py
@@ -1,8 +1,9 @@
-weather = "Summer"
+weather = input("wheather ")
 place = "Sahara"
 grade_of_temp = int(input("temperature in Sahara? "))
 
 if weather == "Summer" :
 	if place == "Sahara" and grade_of_temp < 0:
 	    print("Are you crazy?")
-print("it's ok")
+else:
+	print("I cant tell you anything")
