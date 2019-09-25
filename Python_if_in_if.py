91a3deab5192f7a0a6b988289e68de84d669fc09
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-25 20:05:26 +0400

    If_in_if example

---
+++ Python_if_in_if.py
@@ -0,0 +1,8 @@
+weather = "Summer"
+place = "Sahara"
+grade_of_temp = int(input("temperature in Sahara? "))
+
+if weather == "Summer" :
+	if place == "Sahara" and grade_of_temp < 0:
+	    print("Are you crazy?")
+print("it's ok")
