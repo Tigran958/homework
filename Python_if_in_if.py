e41e7bb8d7371f8ba8e34f30e392b83a4f3b334c
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-25 21:03:13 +0400

    examples with if else and elif

--- Python_if_in_if.py
+++ Python_if_in_if.py
@@ -1,9 +1,26 @@
-weather = input("wheather ")
-place = "Sahara"
-grade_of_temp = int(input("temperature in Sahara? "))
 
-if weather == "Summer" :
-	if place == "Sahara" and grade_of_temp < 0:
-	    print("Are you crazy?")
+first_input = input("Tell me a number ")
+number_1 = int(first_input)
+
+if number_1 >= 0:
+    if number_1 != 0:
+       print("It is positive")
+    else:
+       print("it is 0")
 else:
-	print("I cant tell you anything")
+	print("it is negativ")
+###########second##########
+first_1_input = input("Tell me the temperature and I'll  tell you what to do ")
+temp = int(first_input)
+if temp > 0:
+	if temp > 20:
+		print("It is hot outside, take off your jacket")
+	elif temp > 10:
+		print("You can go out with your jack")
+	else:
+		print("take your coat")
+elif temp < 0 and temp > -10:
+	print("Find somthing to burn fire")
+else:
+	print("what are you doing? go back to your bad")
+
