126f4420d581ffecab68fb546629e2e07b692a74
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-27 01:00:43 +0400

    Homework 27/09/2019

--- Python_homework_Tigran.py
+++ Python_homework_Tigran.py
@@ -1,29 +1,11 @@
 ###########First. Word/sentence/number check#############
-first_print = "Hello, tell me something and I'll try to guess if it is a sentence or a word or a number. "
+first_print = "Hello, tell me something and I'll try to guess if it is a sentence or a word. "
 word_print = "You have input a word"
 sentence_print = "You have input a sentence"
-number_print = "You have input a number"
 pb = " "
-n_0 = "0"
-n_1 = "1"
-n_2 = "2"
-n_3 = "3"
-n_4 = "4"
-n_5 = "5"
-n_6 = "6"
-n_7 = "7"
-n_8 = "8"
-n_9 = "9"
-
 input_first = input(first_print)
-check_number_in = (n_0 in input_first) or (n_1 in input_first) or (n_2 in input_first) or (n_3 in input_first) or (n_4 in input_first) or (n_5 in input_first) or (n_6 in input_first) or (n_7 in input_first) or (n_8 in input_first) or (n_9 in input_first)
 
-if check_number_in:
-	if pb not in input_first:
-	    print(number_print)
-	else:
-	    print(sentence_print) 
-elif pb in input_first:
+if pb in input_first:
     print(sentence_print)
 else:
 	print(word_print)
