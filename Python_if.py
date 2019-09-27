86b4059efdbe402c79eee6cba0966d39b7e21b8f
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-27 19:26:06 +0400

    Range acts

--- Python_if.py
+++ Python_if.py
@@ -1,13 +1,10 @@
-a = 7
-b = 7
-c = a + b
-g = "Hello"
-t = "Hello world"
-if a == b:
-	print("They are equals")
-if a > b:
-	print("a is great")
-if a == b and a < c:
-	print(t)
-if g in t and a == b:
-	print("Aloha")
+for i in range(0, 3):
+    print(i)
+for a in range(1,3):
+	print(a+i)
+for b in range(10):
+   print(b)
+for c in range(0, 10, 2):
+	print(c)
+for g in range(10, 1, -1):
+    print(g)
