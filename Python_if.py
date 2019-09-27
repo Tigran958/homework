f82425a4c333e10e168139dbc70821f43826bfdc
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-27 20:29:51 +0400

    loop in loop

--- Python_if.py
+++ Python_if.py
@@ -1,18 +1,22 @@
 import random
 
+number_of_try = int(input("How many times?"))
+for i in range(0, number_of_try)
 hidden_number = random.randint(1,100)
 
 user_guess = 0
 
-while not user_guess == hidden_number:
+
+	while not user_guess == hidden_number:
 	
-	user_guess = int(input("Guess a number: "))
+		user_guess = int(input("Guess a number: "))
 
-	if user_guess > hidden_number:
-		print("Too high")
+		if user_guess > hidden_number:
+			print("Too high")
 
-	elif user_guess < hidden_number:
-		print("Too low")
+		elif user_guess < hidden_number:
+			print("Too low")
 
-	else:
-		print("Thats right!")
+		else:
+			print("Thats right!")
+
