b49f630aa5e6536d9a995fe57dfb956d97b8f16e
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-27 19:49:40 +0400

    for

--- Python_if.py
+++ Python_if.py
@@ -1,10 +1,8 @@
-for i in range(0, 3):
-    print(i)
-for a in range(1,3):
-	print(a+i)
-for b in range(10):
-   print(b)
-for c in range(0, 10, 2):
-	print(c)
-for g in range(10, 1, -1):
-    print(g)
+sum = 0
+
+for i in range(1,4):
+	next_number = int(input("Enter number # " + str(i) + ": "))
+	sum += next_number
+print(sum/3)
+
+
