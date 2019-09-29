71944a39d7be43d3f36183852617e5675f774c3a
Tigran958 <tigran.danielyan3@gmail.com>
2019-09-30 01:35:01 +0400

    Homework 30/09/2019

--- Python_homework_Tigran.py
+++ Python_homework_Tigran.py
@@ -1,63 +1,38 @@
-###########First. Word/sentence/number check#############
-first_print = "Hello, tell me something and I'll try to guess if it is a sentence or a word. "
-word_print = "You have input a word"
-sentence_print = "You have input a sentence"
-pb = " "
-input_first = input(first_print)
+###########1############
 
-if pb in input_first:
-    print(sentence_print)
-else:
-	print(word_print)
+number_q = input("Enter a number to see the multiplication table: ")
+number_input = int(number_q)
+range_number = range(0,11)
 
+for i in range_number:
+	print(number_q + "*" + str(i) + "=", number_input * i)
 
-##############Second. 5 and 11 div check #################
-input_number = "Tell me a number and I'll tell you wheather it divides by 5 and 11 or not "
-div_5_not_11 = "It is divisible by 5 but not 11"
-div_5_and_11 = "It is divisible by 5 and 11"
-div_11_not_5 = "It is divisible by 11 but not 5"
-div_n_5_n_11 = "It is divisible neither 5 nor 11"
+###########2############
+print("")
+number_1 = input("Enter a number and se the count of digits: ")
+print(len(number_1))
 
-number_check = int(input(input_number))
-check_5 = number_check / 5 
-check_11 = number_check / 11
+############3###########
 
-if check_5 // 1 == check_5:
-	if check_11 // 1 == check_11:
-		print(div_5_and_11)
-	else:
-		print(div_5_not_11)
-elif check_11 // 1 == check_11:
-	print(div_11_not_5)
-else:
-	print(div_n_5_n_11)
+star = "*"
+a_range = range(1,15)
+count = 0 
+first_count = 1
+second_count = 4
 
-#########Third. Leep year check############
-third_print = "Do you want to know is it a leep year? Then input a number "
-leep_print = "This is a leep year"
-no_leep_print = "This is not a leep year"
-year_input = int(input(third_print))
+print("Here we go for third task ")
+print("")
 
-year_4 = (year_input / 4) // 1 == (year_input / 4)
-year_100 = (year_input / 100) // 1 == (year_input / 100)
-year_400 = (year_input / 400) // 1 == (year_input / 400)
+for j in a_range:
+	if count <= 4:
+		print(first_count * star)
+		count += 1
+		first_count += 1
 
-if year_4:
-    if year_400:
-        print(leep_print)
-    elif year_100:
-        print(no_leep_print)
-    else:
-    	print(leep_print)
-else:
-    print(no_leep_print)
-
-
-
-
-
-
-
-	
-
+	elif count > 4:
+		print(second_count * star)
+		second_count -= 1
+		if second_count == 0:
+			break
+	 
 
