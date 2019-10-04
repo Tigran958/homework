039a58a5d0fe5db064ee8febe2db35a3d7f053f5
Tigran958 <tigran.danielyan3@gmail.com>
2019-10-04 20:39:15 +0400

    class04/10/2019

--- class_04_10_19.py
+++ class_04_10_19.py
@@ -15,4 +15,24 @@ except TypeError:
 except:
 	print("Unknown error")
 
-Print("done")			
+
+Print("done")	
+
+try:
+	input_file = open("NumberFile.txt", mode = "r")
+
+	try:
+		for line in input_file:
+			print(int(line))
+
+	except ValueError:
+		print("A value error")
+
+	else:
+		print("No error occured")
+
+	finally:
+		input_file.close()
+
+except IOError:
+	print("An error occured while reading the file")					
