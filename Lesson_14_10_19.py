6822d61e4ee9bc4f2a03fa47efcdb9ff08f4ee59
Tigran958 <tigran.danielyan3@gmail.com>
2019-10-14 19:31:53 +0400

    Dictinories/ acts

---
+++ Lesson_14_10_19.py
@@ -0,0 +1,14 @@
+# human = {'name': 'Tigran', 'surname': 'Danielyan', 'age': 23, 'is_married': False}
+# print(human)
+# print(human['name'], human['age'], human['is_married'])
+
+human = {'name': 'Tigran', 'surname': 'Danielyan', 'age': 23, 'is_married': False}
+print(human)
+
+human['age'] = 30
+
+human['have_pet'] = False
+
+del human['age']
+
+print(human)
