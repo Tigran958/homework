a27d998e81ea33a5c91427c690badb2f7c847887
Tigran958 <tigran.danielyan3@gmail.com>
2019-10-14 19:48:26 +0400

    Dictinories/ acts

--- Lesson_14_10_19.py
+++ Lesson_14_10_19.py
@@ -1,6 +1,6 @@
-# human = {'name': 'Tigran', 'surname': 'Danielyan', 'age': 23, 'is_married': False}
-# print(human)
-# print(human['name'], human['age'], human['is_married'])
+human = {'name': 'Tigran', 'surname': 'Danielyan', 'age': 23, 'is_married': False}
+print(human)
+print(human['name'], human['age'], human['is_married'])
 
 human = {'name': 'Tigran', 'surname': 'Danielyan', 'age': 23, 'is_married': False}
 print(human)
@@ -12,3 +12,16 @@ human['have_pet'] = False
 del human['age']
 
 print(human)
+
+human = {'name': 'Tigran', 'surname': 'Danielyan', 'age': 23, 'is_married': False}
+print(human.keys())
+print(human.values())
+
+for value in human.values():
+	print(value)
+
+fruits = {'apple': 5, 'orange': 14, 'pineapple': 1, 'bananas': 4, 'pomegranade': 6}
+
+for key in fruits.keys():
+	if fruits[key] > 5:
+		print(key)
