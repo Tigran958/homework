e8fc7ec65aa535d9ea35cc1bef76715313d3a2f9
Tigran958 <tigran.danielyan3@gmail.com>
2019-10-19 01:13:08 +0400

    Initialize Bank account and introduction of class/intence

--- OOP.py
+++ OOP.py
@@ -37,29 +37,90 @@
 # print(my_person2.first_name)
 # print(my_person2.last_name)
 
-class BankAccount:
-	def __init__(self, name, balance=0.0):
-		self.log("Account created")
+# class BankAccount:
+# 	def __init__(self, name, balance=0.0):
+# 		self.log("Account created")
+# 		self.name = name
+# 		self.balance = balance
+
+# 	def getBalance(self):
+# 		self.log("Balanc checked at " + str(self.balance))
+# 		return self.balance
+
+# 	def deposit(self, amount):
+# 		self.balance += amount
+# 		self.log("+ " + str(amount) + ": " + str(self.balance))
+
+# 	def withdraw(self, amount):
+# 		self.balance -= amount
+# 		self.log("- " + str(amount) + ": " + str(self.balance))
+
+# 	def log(self, message):
+# 		print(message)
+
+# my_bank_account = BankAccount("Tigran Danielyan")
+# my_bank_account.deposit(20.0)		
+# my_bank_account.getBalance()
+# my_bank_account.withdraw(10.0)
+# my_bank_account.getBalance()
+
+
+# class Person:
+# 	def __init__(self, name, eyecolor, age):
+# 		self.name = name
+# 		self.eyecolor = eyecolor
+# 		self.age = age
+
+# my_person1 = Person("Tigran Danielyan", "Brown", 19)
+
+# my_person3 = Person(my_person1.name, my_person1.eyecolor, my_person1.age)
+
+# print(my_person1.name)		
+# print(my_person3.name)	
+
+# my_person1.name = 'aaaa'
+
+# print(my_person1.name)		
+# print(my_person3.name)
+###########################
+
+# class Name:
+# 	def __init__(self, first_name, last_name):
+# 		self.first_name = first_name
+# 		self.last_name = last_name
+
+# class Person:
+# 	def __init__(self, name, eyecolor, age):
+# 		self.name = name
+# 		self.eyecolor = eyecolor
+# 		self.age = age
+
+# my_name = Name("Tigran", "Danielyan")
+# my_person = Person(my_name, "Brown", 19)
+
+# print(my_person.name)
+# print(my_person.name.first_name)
+# print(my_person.name.last_name)
+# print(my_person.age)
+#############################
+class Name:
+	def __init__(self, first_name, last_name):
+		self.first_name = first_name
+		self.last_name = last_name
+
+class Person:
+	def __init__(self, name, eyecolor, age):
 		self.name = name
-		self.balance = balance
+		self.eyecolor = eyecolor
+		self.age = age
 
-	def getBalance(self):
-		self.log("Balanc checked at " + str(self.balance))
-		return self.balance
+my_name = Name("Tigran", "Danielyan")
+my_person = Person(my_name, "Brown", 19)
 
-	def deposit(self, amount):
-		self.balance += amount
-		self.log("+ " + str(amount) + ": " + str(self.balance))
+def capitalize_name(name):
+	name.first_name = name.first_name.upper()
+	name.last_name = name.last_name.upper()
 
-	def withdraw(self, amount):
-		self.balance -= amount
-		self.log("- " + str(amount) + ": " + str(self.balance))
-
-	def log(self, message):
-		print(message)
-
-my_bank_account = BankAccount("Tigran Danielyan")
-my_bank_account.deposit(20.0)		
-my_bank_account.getBalance()
-my_bank_account.withdraw(10.0)
-my_bank_account.getBalance()
+capitalize_name(my_name)
+print(my_person.name.first_name)
+print(my_person.name.last_name)
