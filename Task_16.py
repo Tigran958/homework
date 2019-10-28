116fcd0dde39a527e19222fd7ffeffaf31a63819
Tigran958 <tigran.danielyan3@gmail.com>
2019-10-28 20:36:57 +0400

    Exam task 16

---
+++ Task_16.py
@@ -0,0 +1,4 @@
+list_1 = ["Oh yeah, I'll tell you somethin'", "I think you'll understand", "When I say that somethin'", "I want to hold your hand", "I want to hold your hand", "I want to hold your hand"]
+
+a = int(input("Tell me the line ")) - 1
+print(list_1[a])
