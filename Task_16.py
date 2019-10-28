63b758ff9ecebce00bfce88c61ac4354ec24f87a
Tigran958 <tigran.danielyan3@gmail.com>
2019-10-28 20:45:46 +0400

    Exam Task 16

--- Task_16.py
+++ Task_16.py
@@ -1,4 +1,13 @@
 list_1 = ["Oh yeah, I'll tell you somethin'", "I think you'll understand", "When I say that somethin'", "I want to hold your hand", "I want to hold your hand", "I want to hold your hand"]
 
-a = int(input("Tell me the line ")) - 1
-print(list_1[a])
+while True:
+	try:
+		a = int(input("Tell me the line ")) - 1
+		print(list_1[a])
+		break
+	except IndexError:
+		print("try again ")
+	except ValueError:
+		print("Enter number ")
+	except:
+		print("again try again")
