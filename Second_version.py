f33d8183e92fc686600970a285bb43a3facd1d21
Tigran958 <tigran.danielyan3@gmail.com>
2019-11-01 18:36:22 +0400

    Homework 31/11/2019 Multy Threading new

--- Second_version.py
+++ Second_version.py
@@ -3,7 +3,7 @@ import datetime
 import threading
 
 
-f = datetime.datetime.now()
+
 # j = M_t_1.calculate()
 # print(b)
 g = threading.Thread(target=M_t_1.calculate)
@@ -11,5 +11,6 @@ g = threading.Thread(target=M_t_1.calculate)
 l = g.start()
 
 while g.is_alive():
+	f = datetime.datetime.now()
 	print(f)
 print(g.is_alive())
